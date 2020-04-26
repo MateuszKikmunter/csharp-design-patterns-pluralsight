5065407ea31fd771bddbf877cf9278def5565ec5
m.kikmunter <M.Kikmunter@gmail.com>
2020-04-26 12:11:22 +0100

    starting to implement the pattern - initial

--- CSharpDesignPatterns/State Design Pattern/Logic/BookingContext.cs
+++ CSharpDesignPatterns/State Design Pattern/Logic/BookingContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using State_Design_Pattern.UI;
+﻿using State_Design_Pattern.UI;
 
 namespace State_Design_Pattern.Logic
 {
