360e330a0788d6da77a3cf41fb8d758ff9eb2e08
m.kikmunter <M.Kikmunter@gmail.com>
2020-04-26 19:51:45 +0100

    added code for stating point

--- CSharpDesignPatterns/StrategyPattern/Program.cs
+++ CSharpDesignPatterns/StrategyPattern/Program.cs
@@ -6,7 +6,19 @@ namespace StrategyPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var order = new Order
+            {
+                ShippingDetails = new ShippingDetails
+                {
+                    OriginCountry = "Sweden",
+                    DestinationCountry = "Sweden"
+                }
+            };
+
+            order.LineItems.Add(new Item("CSHARP_SMORGASBORD", "C# Smorgasbord", 100m, ItemType.Literature), 1);
+            order.LineItems.Add(new Item("CONSULTING", "Building a website", 100m, ItemType.Service), 1);
+
+            Console.WriteLine(order.GetTax());
         }
     }
 }
