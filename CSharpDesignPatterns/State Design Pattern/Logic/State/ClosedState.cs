9dfcf6725ec61b9127624bb0283ca9679740ee66
m.kikmunter <M.Kikmunter@gmail.com>
2020-04-26 12:22:18 +0100

    added initial boiler plate code for required concrete states

---
+++ CSharpDesignPatterns/State Design Pattern/Logic/State/ClosedState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace State_Design_Pattern.Logic
+{
+    public class ClosedState : BookingState
+    {
+        public override void Cancel(BookingContext booking)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DatePassed(BookingContext booking)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void EnterDetails(BookingContext booking, string attendee, int ticketCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void EnterState(BookingContext booking)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
