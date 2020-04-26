61a4fca0e91e5db341aadc0c829098473801bc87
m.kikmunter <M.Kikmunter@gmail.com>
2020-04-26 12:31:22 +0100

    implemented context for state pattern

--- CSharpDesignPatterns/State Design Pattern/Logic/State/BookingContext.cs
+++ CSharpDesignPatterns/State Design Pattern/Logic/State/BookingContext.cs
@@ -5,28 +5,40 @@ namespace State_Design_Pattern.Logic
     public class BookingContext
     {
         public MainWindow View { get; private set; }
+
         public string Attendee { get; set; }
+
         public int TicketCount { get; set; }
+
         public int BookingID { get; set; }
+
+        private BookingState _currentState;
+
+        public void TransitionToState(BookingState state)
+        {
+            _currentState = state;
+            _currentState.EnterState(this);
+        }
 
         public BookingContext(MainWindow view)
         {
             View = view;
+            TransitionToState(new NewState());
         }
 
         public void SubmitDetails(string attendee, int ticketCount)
         {
-            
+            _currentState.EnterDetails(this, attendee, ticketCount);
         }
 
         public void Cancel()
         {
-            
+            _currentState.Cancel(this);
         }
 
         public void DatePassed()
         {
-           
+            _currentState.DatePassed(this);
         }
 
         public void ShowState(string stateName)
