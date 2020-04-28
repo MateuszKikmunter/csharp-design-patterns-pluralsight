652c7422f0fdb5448b5db66c054891c1e1834c7d
m.kikmunter <M.Kikmunter@gmail.com>
2020-04-28 20:38:50 +0100

    added UPSShippingStrategy

---
+++ CSharpDesignPatterns/StrategyPattern/Business/Models/Strategies/Concrete/UPSShippingStrategy.cs
@@ -0,0 +1,13 @@
+﻿using StrategyPattern.Business.Models.Strategies.Abstract;
+using System;
+
+namespace StrategyPattern.Business.Models.Strategies.Concrete
+{
+    public class UPSShippingStrategy : IShippingStrategy
+    {
+        public void Ship(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
