76467bda955c1ac63055b28421d5ea0892de5293
m.kikmunter <M.Kikmunter@gmail.com>
2020-04-28 20:36:57 +0100

    added UnitedStatesPostalServicesShippingStrategy

---
+++ CSharpDesignPatterns/StrategyPattern/Business/Models/Strategies/Concrete/UnitedStatesPostalServicesShippingStrategy.cs
@@ -0,0 +1,12 @@
+﻿using StrategyPattern.Business.Models.Strategies.Abstract;
+
+namespace StrategyPattern.Business.Models.Strategies.Concrete
+{
+    public class UnitedStatesPostalServicesShippingStrategy : IShippingStrategy
+    {
+        public void Ship(Order order)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
