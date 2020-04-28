e94325708b7f55bfa1583d40494ad7230463e024
m.kikmunter <M.Kikmunter@gmail.com>
2020-04-28 20:13:42 +0100

    added PrintOnDemandInvoice - init

---
+++ CSharpDesignPatterns/StrategyPattern/Business/Models/Strategies/Concrete/PrintOnDemandInvoiceStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPattern.Business.Models.Strategies.Concrete
+{
+    class PrintOnDemandInvoiceStrategy
+    {
+    }
+}
