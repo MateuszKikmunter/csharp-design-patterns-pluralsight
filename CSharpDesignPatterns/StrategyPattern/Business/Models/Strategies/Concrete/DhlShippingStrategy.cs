d7f2b0dc6ab655438587879af8c241331617bc8a
m.kikmunter <M.Kikmunter@gmail.com>
2020-04-28 20:31:28 +0100

    added DhlShippingStrategy

---
+++ CSharpDesignPatterns/StrategyPattern/Business/Models/Strategies/Concrete/DhlShippingStrategy.cs
@@ -0,0 +1,12 @@
+﻿using StrategyPattern.Business.Models.Strategies.Abstract;
+
+namespace StrategyPattern.Business.Models.Strategies.Concrete
+{
+    public class DhlShippingStrategy : IShippingStrategy
+    {
+        public void Ship(Order order)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
