214af475d673a297e4a56cb00f1e3dfbf5fb45ee
m.kikmunter <M.Kikmunter@gmail.com>
2020-04-28 20:17:07 +0100

    implemented PrintOnDemandInvoiceStrategy

--- CSharpDesignPatterns/StrategyPattern/Business/Models/Strategies/Concrete/PrintOnDemandInvoiceStrategy.cs
+++ CSharpDesignPatterns/StrategyPattern/Business/Models/Strategies/Concrete/PrintOnDemandInvoiceStrategy.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Newtonsoft.Json;
+using StrategyPattern.Business.Models.Strategies.Abstract;
+using System;
+using System.Net.Http;
 
 namespace StrategyPattern.Business.Models.Strategies.Concrete
 {
-    class PrintOnDemandInvoiceStrategy
+    public class PrintOnDemandInvoiceStrategy : IInvoiceStrategy
     {
+        public void Generate(Order order)
+        {
+            using (var client = new HttpClient())
+            {
+                var content = JsonConvert.SerializeObject(order);
+                client.BaseAddress = new Uri("https://pluralsight.com");
+                client.PostAsync("/print-on-demand", new StringContent(content));
+            }
+        }
     }
 }
