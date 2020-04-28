a4818747cfc816ad710cad0b9d9694425aa9c615
m.kikmunter <M.Kikmunter@gmail.com>
2020-04-28 20:11:27 +0100

    added file invoice strategy - init

---
+++ CSharpDesignPatterns/StrategyPattern/Business/Models/Strategies/Concrete/FileInvoiceStrategy.cs
@@ -0,0 +1,12 @@
+﻿using StrategyPattern.Business.Models.Strategies.Abstract;
+
+namespace StrategyPattern.Business.Models.Strategies.Concrete
+{
+    public class FileInvoiceStrategy : InvoiceStrategy
+    {
+        public override void Generate(Order order)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
