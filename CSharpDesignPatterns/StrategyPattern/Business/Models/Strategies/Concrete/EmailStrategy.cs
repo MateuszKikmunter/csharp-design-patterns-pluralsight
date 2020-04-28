5f37d73b118729eb660d7e702fe3a45ce9c19194
m.kikmunter <M.Kikmunter@gmail.com>
2020-04-28 16:53:55 +0100

    implemented email strategy

---
+++ CSharpDesignPatterns/StrategyPattern/Business/Models/Strategies/Concrete/EmailStrategy.cs
@@ -0,0 +1,28 @@
+﻿using StrategyPattern.Business.Models.Strategies.Abstract;
+using System.Net;
+using System.Net.Mail;
+
+namespace StrategyPattern.Business.Models.Strategies.Concrete
+{
+    public class EmailStrategy : InvoiceStrategy
+    {
+        public override void Generate(Order order)
+        {
+            var body = GenerateTextInvoice(order);
+
+            using (var client = new SmtpClient("smtp.sendgrid.net", 587))
+            {
+                var credentials = new NetworkCredential("your-user-name", "your-password");
+                client.Credentials = credentials;
+
+                var mail = new MailMessage("your-email-address", "recipient-email")
+                {
+                    Subject = "We've created an invoice for your order.",
+                    Body = body
+                };
+
+                client.Send(mail);
+            }
+        }
+    }
+}
