b1c6d1529537f337248d35316948bd8a17470b7a
m.kikmunter <M.Kikmunter@gmail.com>
2020-04-28 20:35:10 +0100

    added SwedishPostalServiceShippingStrategy

---
+++ CSharpDesignPatterns/StrategyPattern/Business/Models/Strategies/Concrete/SwedishPostalServiceShippingStrategy.cs
@@ -0,0 +1,12 @@
+﻿using StrategyPattern.Business.Models.Strategies.Abstract;
+
+namespace StrategyPattern.Business.Models.Strategies.Concrete
+{
+    public class SwedishPostalServiceShippingStrategy : IShippingStrategy
+    {
+        public void Ship(Order order)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
