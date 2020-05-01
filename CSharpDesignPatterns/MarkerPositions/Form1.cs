4447d59642906aa68c0f325e48aa952fbc4097b5
m.kikmunter <M.Kikmunter@gmail.com>
2020-05-01 18:21:23 +0100

    implemented form

--- CSharpDesignPatterns/MarkerPositions/Form1.cs
+++ CSharpDesignPatterns/MarkerPositions/Form1.cs
@@ -1,13 +1,28 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace MarkerPositions
 {
     public partial class Form1 : Form
     {
+        private readonly MarkerMediator _mediator = new MarkerMediator();
+
+        private Button addButton;
+
         public Form1()
         {
             InitializeComponent();
+            addButton = new Button();
+            addButton.Click += OnAddClick;
+            addButton.Text = "Add Marker";
+            addButton.Dock = DockStyle.Bottom;
+            Controls.Add(addButton);
         }
 
+        private void OnAddClick(object sender, EventArgs e)
+        {
+            var marker = _mediator.CreateMarker();
+            Controls.Add(marker);
+        }
     }
 }
