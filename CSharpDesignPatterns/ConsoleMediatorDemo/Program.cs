3b2f9ffe53813abed0499f08122647d6c025b283
m.kikmunter <M.Kikmunter@gmail.com>
2020-05-01 15:25:31 +0100

    mediator pattern init - console app demo

---
+++ CSharpDesignPatterns/ConsoleMediatorDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleMediatorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
