95a5179bdcbf31426907bd6a4cbb795fa0f3e6c4
m.kikmunter <M.Kikmunter@gmail.com>
2020-05-01 15:33:42 +0100

    added concrete colleagues

---
+++ CSharpDesignPatterns/ConsoleMediatorDemo/Structural/ConcreteColleague.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleMediatorDemo.Structural
+{
+    public class ConcreteColleague : Colleague
+    {
+        public ConcreteColleague(Mediator mediator) : base(mediator)
+        {
+        }
+
+        public override void HandleNotification(string message)
+        {
+            Console.WriteLine($"Conrecte colleague receives notification: { message }");
+        }
+    }
+}
---
+++ CSharpDesignPatterns/ConsoleMediatorDemo/Structural/SecondConreteColleage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleMediatorDemo.Structural
+{
+    public class SecondConreteColleage : Colleague
+    {
+        public SecondConreteColleage(Mediator mediator) : base(mediator)
+        {
+        }
+
+        public override void HandleNotification(string message)
+        {
+            Console.WriteLine($"Second concrete colleague receives notifiacation: { message }");
+        }
+    }
+}
