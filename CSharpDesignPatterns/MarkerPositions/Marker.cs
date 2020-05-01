f18ad80360e3144f17094e7fd1d78a8b9be8685e
m.kikmunter <M.Kikmunter@gmail.com>
2020-05-01 18:12:01 +0100

    implemented mediator and colleague

---
+++ CSharpDesignPatterns/MarkerPositions/Marker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MarkerPositions
+{
+    public class Marker : Label
+    {
+        private MarkerMediator _mediator;
+
+        private Point _mouseDownLocation;
+
+        public Marker()
+        {
+            Text = "{Drag me}";
+            TextAlign = ContentAlignment.MiddleCenter;
+            MouseDown += OnMouseDown;
+            MouseMove += OnMouseMove;
+        }
+
+        public void SetMediator(MarkerMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public void ReceiveLocation(Point location)
+        {
+            var distance = CalcDistance(location);
+            if (distance < 100 && BackColor != Color.Red)
+            {
+                BackColor = Color.Red;
+            }
+            else if (distance >= 100 && BackColor != Color.Green)
+            {
+                BackColor = Color.Green;
+            }
+
+            double CalcDistance(Point point) => Math.Sqrt(Math.Pow(point.X - Location.X, 2) + Math.Pow(point.Y - Location.Y, 2));
+        }
+
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _mouseDownLocation = e.Location;
+            }
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Text = Location.ToString();
+                Left = e.X + Left - _mouseDownLocation.X;
+                Top = e.Y + Top - _mouseDownLocation.Y;
+                _mediator.Send(Location, this);
+            }
+        }
+    }
+}
---
+++ CSharpDesignPatterns/MarkerPositions/MarkerMediator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace MarkerPositions
+{
+    public class MarkerMediator
+    {
+        private readonly List<Marker> _markers = new List<Marker>();
+
+        public Marker CreateMarker()
+        {
+            var marker = new Marker();
+            marker.SetMediator(this);
+            return marker;
+        }
+
+        public void Send(Point location, Marker marker)
+        {
+            _markers.Where(m => m != marker).ToList().ForEach(m => m.ReceiveLocation(location));
+        }
+    }
+}
