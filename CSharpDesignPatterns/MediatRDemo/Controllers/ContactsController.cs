1f50fc20b0f4a7d0cc3896114c36e2d0b8d12e58
m.kikmunter <M.Kikmunter@gmail.com>
2020-05-01 19:02:16 +0100

    contacts controller init

---
+++ CSharpDesignPatterns/MediatRDemo/Controllers/ContactsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MediatRDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+    }
+}
