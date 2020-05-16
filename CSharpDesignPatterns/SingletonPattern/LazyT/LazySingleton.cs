e1ad30c0a438cda614d7ce1683958497979c45dc
m.kikmunter <M.Kikmunter@gmail.com>
2020-05-16 17:25:17 +0100

    added lazt of t singleton

---
+++ CSharpDesignPatterns/SingletonPattern/LazyT/LazySingleton.cs
@@ -0,0 +1,24 @@
+﻿using SingletonPattern.Utils;
+using System;
+
+namespace SingletonPattern.LazyT
+{
+    public sealed class LazySingleton
+    {
+        public static readonly Lazy<LazySingleton> _lazy = new Lazy<LazySingleton>(() => new LazySingleton());
+
+        public static LazySingleton Instance
+        {
+            get
+            {
+                Logger.Log("Instance called!");
+                return _lazy.Value;
+            }
+        }
+
+        private LazySingleton()
+        {
+            Logger.Log("Constructor called!");
+        }
+    }
+}
