9431d8d192283a45080733b4c73732126aa7deab
m.kikmunter <M.Kikmunter@gmail.com>
2020-05-16 17:01:31 +0100

    added Singleton with lock to have thread safety

---
+++ CSharpDesignPatterns/SingletonPattern/Locking/SingletonWithLocking.cs
@@ -0,0 +1,31 @@
+﻿using SingletonPattern.Utils;
+
+namespace SingletonPattern.Locking
+{
+    public sealed class SingletonWithLocking
+    {
+        private static SingletonWithLocking? _instance = null;
+        private static readonly object _padlock = new object();
+
+        public static SingletonWithLocking Instance
+        {
+            get
+            {
+                Logger.Log("Calling instance!");
+                lock(_padlock) // this lock is used on every reference to Singleton
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new SingletonWithLocking();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        private SingletonWithLocking()
+        {
+            Logger.Log("Calling constructor!");
+        }
+    }
+}
