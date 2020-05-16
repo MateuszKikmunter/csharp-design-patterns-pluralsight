4e12edbe6b33bfb05ea472f946e4025978377f87
m.kikmunter <M.Kikmunter@gmail.com>
2020-05-16 16:45:43 +0100

    added tests helper

---
+++ CSharpDesignPatterns/SingletonPattern/Utils/SingletonTestHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace SingletonPattern.Utils
+{
+    public static class SingletonTestHelpers
+    {
+        public static void Reset(Type type)
+        {
+            var info = type.GetField("_instance", BindingFlags.NonPublic | BindingFlags.Static);
+            info.SetValue(null, null);
+        }
+
+        public static T GetPrivateStaticInstance<T>() where T : class
+        {
+            var type = typeof(T);
+            var info = type.GetField("_instance", BindingFlags.NonPublic | BindingFlags.Static);
+            return info.GetValue(null) as T;
+        }
+    }
+}
