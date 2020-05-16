7d998f4269f985e5f303511259cb3f39a7dd9099
m.kikmunter <M.Kikmunter@gmail.com>
2020-05-16 17:18:41 +0100

    static constructor singleton

---
+++ CSharpDesignPatterns/SingletonPattern/StaticConstructor/StaticConstructorSingleton.cs
@@ -0,0 +1,26 @@
+﻿using SingletonPattern.Utils;
+
+namespace SingletonPattern.StaticConstructor
+{
+    public sealed class StaticConstructorSingleton
+    {
+        private static readonly StaticConstructorSingleton _instance = new StaticConstructorSingleton();
+
+        //reading this will intialize the _instance
+        public static readonly string GREETING = "HI!";
+
+        public static StaticConstructorSingleton Instance
+        {
+            get
+            {
+                Logger.Log("Instance called!");
+                return _instance;
+            }
+        }
+
+        static StaticConstructorSingleton()
+        {
+            Logger.Log("Constructor called!");
+        }
+    }
+}
